1cfbe0341f2ff05fa44fa6e899659ed73b870e43
Toby Jennings <tobyj@slac.stanford.edu>
2025-05-12 09:25:19 -0500

    DM-50783: Reuse DRP stage 1 in M49 FL CI

--- M49_FL_CI/{{ cookiecutter.working_dir }}/wrapper.sh
+++ M49_FL_CI/{{ cookiecutter.working_dir }}/wrapper.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 source ./common.sh
 source ./setup_stack.sh
 
-./submit_workflow_stage.sh bps_M49_FL_CI-stage1.yaml && \
+# ./submit_workflow_stage.sh bps_M49_FL_CI-stage1.yaml && \
 ./submit_workflow_stage.sh bps_M49_FL_CI-stage2.yaml && \
 ./submit_workflow_stage.sh bps_M49_FL_CI-stage3.yaml && \
 ./generate_hips_maps.sh
