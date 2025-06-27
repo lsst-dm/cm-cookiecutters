b0375d4861988d91dd4817e263153c3cda3b6f40
Homer Neal <homer@slac.stanford.edu>
2025-06-27 07:31:06 -0700

    skip HiPS maps generation until parallelization has been implimented

--- NightlyValidation/{{ cookiecutter.working_dir }}/wrapper.sh
+++ NightlyValidation/{{ cookiecutter.working_dir }}/wrapper.sh
@@ -4,6 +4,6 @@ source ./common.sh
 source ./setup_stack.sh
 
 ./submit_workflow_stage.sh bps_Nightly-Validation-stage1.yaml && \
-./submit_workflow_stage.sh bps_Nightly-Validation-stage2.yaml && \
-#./submit_workflow_stage.sh bps_Nightly-Validation-stage3.yaml && \
-./generate_hips_maps.sh
+./submit_workflow_stage.sh bps_Nightly-Validation-stage2.yaml
+#./submit_workflow_stage.sh bps_Nightly-Validation-stage2.yaml && \
+#./generate_hips_maps.sh
